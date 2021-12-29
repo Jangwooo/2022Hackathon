c311e36664e9b7a3dc088e2119447c8153250311
jangwoo kim <kjw2262@gmail.com>
2021-12-29 11:51:41 +0900

    리퀘스트 수정

--- interner/domain/object/request/sign_in.go
+++ interner/domain/object/request/sign_in.go
@@ -1,8 +1,6 @@
 package request
 
 type SingIn struct {
-	ID          string `json:"id,omitempty" binding:"required"`
-	Password    string `json:"password,omitempty" binding:"required"`
-	Name        string `json:"name,omitempty" binding:"required"`
-	PhoneNumber string `json:"phone_number,omitempty" binding:"required"`
+	ID       string `json:"id,omitempty" binding:"required"`
+	Password string `json:"password,omitempty" binding:"required"`
 }
